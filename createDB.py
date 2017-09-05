d8a2d11cfdaac46ad46193f63dedba43261bcfe5
Harvey Lieberman <Harvey.Lieberman@gmail.com>
2017-09-05 17:05:10 -0400

    Read in reaction class file

---
+++ createDB.py
@@ -0,0 +1,24 @@
+import re
+
+# Read in rclass file
+file = open("C:/Databases/KEGG/rclass/rclass", "r")
+print("Reading File")
+rclass = file.readlines()
+rclass = rclass[0:1000]
+
+# Find end of each record
+endList = []
+for line_i, line in enumerate(rclass, 1):
+    if re.match('///', line):
+        endList.append(line_i)
+
+# Loop through each record and extract reaction class data
+print("Parsing Data")
+count2 = 0
+for ref in range(0, len(endList)-1):
+    count1 = count2
+    count2 = endList[ref]
+    r = rclass[count1:count2]
+    print(count1)
+    print(r)
+
