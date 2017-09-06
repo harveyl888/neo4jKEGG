42fe682290ae55d8c3e0ea08a3875ae91a494085
Harvey Lieberman <Harvey.Lieberman@gmail.com>
2017-09-06 08:36:15 -0400

    Extract entry and definition

--- createDB.py
+++ createDB.py
@@ -9,7 +9,7 @@ rclass = rclass[0:1000]
 # Find end of each record
 endList = []
 for line_i, line in enumerate(rclass, 1):
-    if re.match('///', line):
+    if re.match("///", line):
         endList.append(line_i)
 
 # Loop through each record and extract reaction class data
@@ -19,10 +19,27 @@ for ref in range(0, len(endList)-1):
     count1 = count2
     count2 = endList[ref]
     r = rclass[count1:count2]
-    tokenList = ['ENTR', 'DEFI', 'RPAI', 'PATH']
-    tokens = {}
+    tokenList = ["ENTR", "DEFI", "RPAI", "PATH"]
+    tokens = dict()
     for line_i, line in enumerate(r, 1):
         if line[0:4] in tokenList:
-            tokens[line[0:4]] = line_i
-    print(tokens)
+            tokens[line[0:4]] = line_i - 1
+    if "ENTR" in tokens.keys():
+        record = dict()
+        record["entry"] = r[tokens["ENTR"]][12:19]
+        if "DEFI" in tokens.keys():
+            foundEnd = False
+            count = 0
+            v_definition = []
+            while not foundEnd:
+                currentLine = r[tokens["DEFI"] + count]
+                if currentLine[0:4] in ["DEFI", "    "]:
+                    v_definition.append(currentLine[12:])
+                    count += 1
+                else:
+                    foundEnd = True
+            record['definition'] = v_definition
+
+
+
 
